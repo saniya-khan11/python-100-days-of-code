f25b6ec0dd95e2a62f45c33fbcf5f65bdfc91491
Saniya-innovates <146875076+saniya-khan11@users.noreply.github.com>
2025-01-13 17:11:13 +0530

    updating calculator project by addinng functions in dictionary

--- project10-calculator.py
+++ project10-calculator.py
@@ -7,34 +7,29 @@ def multiply(n1,n2):
 def divide(n1,n2):
     return n1/n2
 choice="n"
+#adding functions in dictionary and assigning them to dictionary's key
+operations ={
+    "+":add,
+    "-": subtract,
+    "*" : multiply,
+    "/" : divide
+ }
 
 while choice == 'y' or choice=='n':
     if choice =='n':
         n1= float(input("What's the first number?: "))
-        print("+ \n- \n* \n/")
-        operation=input("Pick an operation: ")
+        for symbol in operations:
+            print(symbol)
+        typeOfOperation =input("Pick an operation: ")
         n2= float(input("What's the next number?: "))
-        if operation == '+':
-            result = add(n1,n2)
-        elif operation == '-':
-            result = subtract(n1,n2)
-        elif operation == '*':
-            result = multiply(n1,n2)
-        else:
-            result = divide(n1,n2)
-        print(f"{n1} {operation} {n2} = {result}")
+        result = operations[typeOfOperation](n1,n2)
+        print(f"{n1} {typeOfOperation} {n2} = {result}")
     elif choice == 'y':
         n1=result
-        operation=input("Pick an operation: ")
+        typeOfOperation=input("Pick an operation: ")
         n2= float(input("What's the next number?: "))
-        if operation == '+':
-            result = add(n1,n2)
-        elif operation == '-':
-            result = subtract(n1,n2)
-        elif operation == '*':
-            result = multiply(n1,n2)
-        else:
-            result = divide(n1,n2)
-        print(f"{n1} {operation} {n2} = {result}")
+        result = operations[typeOfOperation](n1,n2)
+        
+        print(f"{n1} {typeOfOperation} {n2} = {result}")
     choice=input(f"Type 'y' to continue calculating with {result} or type 'n' to start a new calculation.\n").lower()
 
