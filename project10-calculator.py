78eaeb26cbe6c86cc14556113f64d9bb81d5f800
Saniya-innovates <146875076+saniya-khan11@users.noreply.github.com>
2025-01-13 17:40:48 +0530

    Update project10-calculator.py

--- project10-calculator.py
+++ project10-calculator.py
@@ -1,3 +1,5 @@
+from art_calculator import logo
+
 def add(n1,n2):
     return n1+n2
 def subtract(n1,n2):
@@ -6,7 +8,7 @@ def multiply(n1,n2):
     return n1*n2
 def divide(n1,n2):
     return n1/n2
-choice="n"
+
 #adding functions in dictionary and assigning them to dictionary's key
 operations ={
     "+":add,
@@ -14,22 +16,24 @@ operations ={
     "*" : multiply,
     "/" : divide
  }
-
-while choice == 'y' or choice=='n':
-    if choice =='n':
-        n1= float(input("What's the first number?: "))
-        for symbol in operations:
-            print(symbol)
-        typeOfOperation =input("Pick an operation: ")
-        n2= float(input("What's the next number?: "))
-        result = operations[typeOfOperation](n1,n2)
-        print(f"{n1} {typeOfOperation} {n2} = {result}")
-    elif choice == 'y':
-        n1=result
-        typeOfOperation=input("Pick an operation: ")
-        n2= float(input("What's the next number?: "))
-        result = operations[typeOfOperation](n1,n2)
-        
-        print(f"{n1} {typeOfOperation} {n2} = {result}")
-    choice=input(f"Type 'y' to continue calculating with {result} or type 'n' to start a new calculation.\n").lower()
+def calculator():
+            print(logo)
+            should_acumulate =True
+            n1= float(input("What's the first number?: "))
+            while should_acumulate:
+                for symbol in operations:
+                    print(symbol)
+                typeOfOperation =input("Pick an operation: ")
+                n2= float(input("What's the next number?: "))
+                result = operations[typeOfOperation](n1,n2)
+                print(f"{n1} {typeOfOperation} {n2} = {result}")
+                choice=input(f"Type 'y' to continue calculating with {result} or type 'n' to start a new calculation.\n").lower()
+                if choice=="y":
+                    n1=result
+                else :
+                    should_acumulate =False
+                    print("\n"*50)
+                    calculator()
+calculator()           
+            
 
