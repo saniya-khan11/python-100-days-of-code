a6c8b177c4d8d519c73044445bbc6bcf1342ff0c
Saniya-innovates <146875076+saniya-khan11@users.noreply.github.com>
2025-04-24 16:28:28 +0530

    General leap year program

---
+++ leap-year.py
@@ -0,0 +1,14 @@
+'''
+Write a program that returns True or False whether if a given year is a leap year.
+'''
+def is_leap_year(year):
+    if year % 4 == 0:
+        if year % 100 == 0:
+            if year % 400 == 0:
+                return True
+            else:
+                return False
+        else:
+            return True
+    else:
+        return False
